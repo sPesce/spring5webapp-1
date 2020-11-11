35016f03a5b2862731b4969c134af277a3a86935
Steve <stevepesce879@gmail.com>
2020-11-11 14:42:47 -0500

    more testing

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -9,6 +9,9 @@ import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by jt on 12/23/19.
  */
@@ -29,38 +32,34 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        Publisher penguin = new Publisher("Penguin", "200 Red st", "Phila", "Pa", "22012" );
+        publisherRepository.save(penguin);
 
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+        ddd.setPublisher(penguin);
+        penguin.getBooks().add(ddd);
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(penguin);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "3939459459");
-        rod.getBooks().add(noEJB);
+
         noEJB.getAuthors().add(rod);
+        noEJB.setPublisher(penguin);
+        penguin.getBooks().add(noEJB);
 
         authorRepository.save(rod);
         bookRepository.save(noEJB);
-
-        Publisher penguin = new Publisher("Penguin", "200 Red st", "Phila", "Pa", "22012" );
         publisherRepository.save(penguin);
-
-        Publisher test = new Publisher();
-        test.setName("Test Publishing");
-        test.setCity("Fakeseville");
-        test.setAddressLine1("123 Fakeington Rd");
-        test.setState("AZ");
-        test.setZip("32211");
-        publisherRepository.save(test);
 
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
         System.out.println("Number of Publishers: " + publisherRepository.count());
-        publisherRepository.findAll().forEach(publisher -> System.out.println("Found publisher " + publisher.getName()));
-
+        System.out.println("Penguin Books: " + penguin.getBooks().size());
     }
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -1,6 +1,8 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by SP 11/11/20
@@ -18,6 +20,10 @@ public class Publisher {
     private String state;
     private String zip;
 
+    @OneToMany
+    @JoinColumn(name = "publisher_id")
+    private Set<Book> books = new HashSet<>();
+
     public Publisher() {
     }
 
@@ -27,6 +33,14 @@ public class Publisher {
         this.city = city;
         this.state = state;
         this.zip = zip;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     public Long getId() {
